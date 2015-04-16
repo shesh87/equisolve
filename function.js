4c86e337ed9a91c6919fdf7944d160a79b7abfe2
Sheonna Harris <svonharris@gmail.com>
2015-04-16 19:05:28 -0400

    Created for loop to push all JSON objects into array then forEached the array to print.

--- function.js
+++ function.js
@@ -15,8 +15,25 @@ function feed() {
 		console.log(data);
 		console.log(data.news[0].title);
 		console.log(data.news[0].published);
-		msg.forEach(function(data){
-			
+		// data.forEach(function(data) {
+		// 	$('.js-feed-posts').append("<li>" + data.data.news[0].title + "</li>");
+		// });
+		// for(var i =0; i < data.length; i++) {
+		// 	$('.js-feed-posts').append("<li>" + "cheese" + "</li>");
+		// }
+		var books = [];
+		for (var i=0; i<data.news.length; i++) {
+			var booksDescript = books.push(data.news[i]);
+		}
+		console.log(books);
+	
+		books.forEach(function(title) {
+			$('.js-feed-posts').append("<li>" + title.title + "</li>");
 		});
 	});
 }
+
+
+
+
+
