7513c1a88e9b28d680cf2dd717ba38d9d8899046
Sheonna Harris <svonharris@gmail.com>
2015-04-16 23:55:53 -0400

    Published dates in array by descending date.

--- function.js
+++ function.js
@@ -17,14 +17,19 @@ function feed() {
 		console.log(data.news[0].published);
 
 		var books = [];
+		var dates = [];
 		for (var i=0; i<data.news.length; i++) {
 			books.push(data.news[i]);
+			dates.push(data.news[i].published);
 		}
 		console.log(books);
+		console.log(dates);
 	
+		dates.sort(function(a,b){return b - a});
 		books.forEach(function(title) {
 			$('.js-feed-posts').append("<li>" + title.title +"<br>"+ "Published in:" +" "+ title.published + "</li>");
 		});
+
 	});
 }
 
