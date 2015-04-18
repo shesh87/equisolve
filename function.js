ffc4716b177055e1e9f2f50d6dc69b16ee39ac89
Sheonna Harris <svonharris@gmail.com>
2015-04-18 06:59:01 -0400

    wrote associative array to pair dates and titles.

--- function.js
+++ function.js
@@ -15,23 +15,28 @@ function feed() {
 		console.log(data);
 		console.log(data.news[0].title);
 		console.log(data.news[0].published);
-
-		var books = [];
-		var dates = [];
-		for (var i=0; i<data.news.length; i++) {
-			books.push(data.news[i]);
-			dates.push(data.news[i].published);
-		}
-		console.log(books);
-		console.log(dates);
 	
-		dates.sort(function(a,b){return b - a});
-		books.forEach(function(title) {
-			$('.js-feed-posts').append("<li>" + title.title +"<br>"+ "Published in:" +" "+ title.published + "</li>");
-		});
+		// dates.sort(function(a,b){return b - a});
+		// books.forEach(function(title) {
+		// 	$('.js-feed-posts').append("<li>" + title.title +"<br>"+ "Published in:" +" "+ title.published + "</li>");
+		// });
 
+		var publishedBooks = {};
+		for (var i=0; i<data.news.length; i++) {
+			var dates = data.news[i].published;
+			var books = data.news[i].title;
+			publishedBooks[dates] = books;
+		}
+console.log(publishedBooks);
+console.log(publishedBooks['2015-04-01 02:00:00']);
 	});
 }
+
+
+
+
+
+
 
 
 
